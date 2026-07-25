a7eef59c845e585d8953105a365a9353ba62c77f
Malek <malek.mohammad132@gmail.com>
2026-07-25 10:47:13 +0330

    Latex

--- Test/test_simulate_steps.m
+++ Test/test_simulate_steps.m
@@ -5,7 +5,7 @@ config('init');
 
 % No params needed here
 x0 = make_initial_state();
-
+animate_rabbit(x0)
 % Controller is now decoupled from the workspace state (pass [] = passive/no
 % torque, so this is a smoke test of the hybrid integration + impact/reset).
 [t, x] = simulate_n_steps(x0, 10, []);
